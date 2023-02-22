49057f84fc1e64f1b2c59a55d56e4e9613f70acd
bishowpratap7 <bishowpratappandey@gmail.com>
2023-02-22 08:30:16 -0500

    Wip

---
+++ algocheck/src/main/java/com/alocheck/algocheck/AlgocheckApplication.java
@@ -0,0 +1,13 @@
+package com.alocheck.algocheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgocheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgocheckApplication.class, args);
+	}
+
+}
